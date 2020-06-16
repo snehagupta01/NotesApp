b2dc39f6db26917abff72f9dfcde71ac03d5e9c7
snehagupta01 <sneha2001gupta@gmail.com>
2020-06-16 15:26:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes=new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView=(ListView)findViewById(R.id.listview);
+        notes.add("Hey there !");
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+
+        listView.setAdapter(arrayAdapter);
+
+       
+    }
+}
